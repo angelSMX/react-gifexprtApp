f1643c43cd14ea69620d088204d6bc851b7e5f60
Angel <angel@Angels-MacBook-Pro.local>
2020-10-26 17:12:37 -0600

    add docs

---
+++ docs/precache-manifest.0f938292c01bfebcf194c7570758380a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20c5b5fee898deb2e1ea86fca96fa907",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5188388cb761355eca94",
+    "url": "/static/css/main.bc0d4a55.chunk.css"
+  },
+  {
+    "revision": "a13e31d6699d5138f112",
+    "url": "/static/js/2.bed60b36.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.bed60b36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5188388cb761355eca94",
+    "url": "/static/js/main.e7d5803a.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
